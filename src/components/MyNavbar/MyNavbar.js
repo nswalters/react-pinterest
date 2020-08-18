228dd6c66d8a8649f83b8370a0269c59255ce553
Nick Walters <nswalters.s@gmail.com>
2020-08-17 21:13:44 -0500

    Logout button

--- src/components/MyNavbar/MyNavbar.js
+++ src/components/MyNavbar/MyNavbar.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import PropTypes from 'prop-types';
+
+import Auth from '../Auth/Auth';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool.isRequired,
+  }
+
+  logoutClickEvent = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
   render() {
     const { authed } = this.props;
 
     const loadComponent = () => {
       if (authed) {
         return (
-          <button className="btn btn-secondary ml-auto">Log Out</button>
+          <button className="btn btn-secondary ml-auto" onClick={this.logoutClickEvent}>Log Out</button>
         );
       }
 
-      return null;
+      return <Auth />;
     };
 
     return (
